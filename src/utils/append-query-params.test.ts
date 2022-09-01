e1fe28f47ac8e306aae8e7b01aec37c14524e6ae
johannes-lindgren <johannes-lindgren@hotmail.com>
2022-09-01 18:32:51 +0200

    test: append-query-params.ts

---
+++ src/utils/append-query-params.test.ts
@@ -0,0 +1,63 @@
+import {appendQueryParams} from "./append-query-params";
+
+it('should work for one query parameter', () => {
+    expect(
+      appendQueryParams(
+        'https://test.com',
+        {
+            page: '1',
+        }
+      )).toBe('https://test.com/?page=1')
+})
+
+it('should work for multiple query parameters', () => {
+    expect(
+      appendQueryParams(
+        'https://test.com',
+        {
+            page: '1',
+            per_page: '12'
+        }
+      )).toBe('https://test.com/?page=1&per_page=12')
+})
+
+it('should work for relative URLs', () => {
+    expect(
+      appendQueryParams(
+        'pages',
+        {
+            id: '123',
+        }
+      )).toBe('pages?id=123')
+})
+
+it('should work for absolute URLs', () => {
+    expect(
+      appendQueryParams(
+        '/pages',
+        {
+            page: '1',
+        }
+      )).toBe('/pages?page=1')
+})
+
+it('should append query parameters', () => {
+    expect(
+      appendQueryParams(
+        '/articles?page=1',
+        {
+            per_page: '12',
+        }
+      )).toBe('/articles?page=1&per_page=12')
+})
+
+it('should replace query parameters', () => {
+    expect(
+      appendQueryParams(
+        '/articles?page=1',
+        {
+            page: '2',
+            per_page: '12',
+        }
+      )).toBe('/articles?page=2&per_page=12')
+})
--- src/utils/append-query-params.ts
+++ src/utils/append-query-params.ts
@@ -1,17 +1,26 @@
 const dummyOrigin = 'https://dummy.com'
 
+/**
+ * Appends query parameters to a URL. Replaces existing parameters.
+ * @param url
+ * @param params
+ */
 export const appendQueryParams = (url: string, params: Record<string, string>): string => {
-    const urlObj = new URL(url, dummyOrigin)
-    Object.entries(params).forEach(([key, value]) => {
-        urlObj.searchParams.set(key, value)
-    })
+  const hasLeadingSlash = url.startsWith('/')
 
-    return (
-        urlObj
-            .toString()
-            .replace(
-                new RegExp(`^${dummyOrigin}`),
-                ''
-            )
+  const urlObj = new URL(url, dummyOrigin)
+  Object.entries(params).forEach(([key, value]) => {
+    urlObj.searchParams.set(key, value)
+  })
+
+  const urlWithLeadingSlash = urlObj
+    .toString()
+    .replace(
+      new RegExp(`^${dummyOrigin}`),
+      ''
     )
+
+  return hasLeadingSlash ? urlWithLeadingSlash : (
+    urlWithLeadingSlash.replace(/^\//, '')
+  )
 }
