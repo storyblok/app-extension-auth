21accedaa8bdcd56e0afb7108b50d8e9792747e0
johannes-lindgren <14206504+johannes-lindgren@users.noreply.github.com>
2023-05-09 09:16:43 +0200

    trimSlashes.ts always returns string

--- src/utils/trimSlashes/trimSlashes.ts
+++ src/utils/trimSlashes/trimSlashes.ts
@@ -1,2 +1,6 @@
+/**
+ * Removes leading and trailing slashes (`/`) from a string.
+ * @param slugs
+ */
 export const trimSlashes = (slugs: string): string =>
   slugs.match(/^\/*(.*?)\/*$/)?.[1] ?? ''
