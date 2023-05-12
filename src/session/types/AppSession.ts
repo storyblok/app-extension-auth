9a35caa6ae896408edb1523c1916530958f91f76
johannes-lindgren <14206504+johannes-lindgren@users.noreply.github.com>
2023-05-12 17:30:52 +0200

    Adding Region to AppSession

--- src/session/types/AppSession.ts
+++ src/session/types/AppSession.ts
@@ -1,3 +1,6 @@
+import { isRegion, Region } from './Region'
+import { hasKey } from '../../utils'
+
 export type AppSession = {
   spaceId: number // primary key
   userId: number // primary key
@@ -8,4 +11,28 @@ export type AppSession = {
   expiresAt: number
   refreshToken: string
   accessToken: string
+  region: Region
 }
+
+export const isAppSession = (data: unknown): data is AppSession =>
+  hasKey(data, 'spaceId') &&
+  typeof data.spaceId === 'number' &&
+  hasKey(data, 'userId') &&
+  typeof data.userId === 'number' &&
+  hasKey(data, 'appClientId') &&
+  typeof data.appClientId === 'string' &&
+  hasKey(data, 'userName') &&
+  typeof data.userName === 'string' &&
+  hasKey(data, 'spaceName') &&
+  typeof data.spaceName === 'string' &&
+  hasKey(data, 'roles') &&
+  Array.isArray(data.roles) &&
+  data.roles.every((role) => typeof role === 'string') &&
+  hasKey(data, 'expiresAt') &&
+  typeof data.expiresAt === 'number' &&
+  hasKey(data, 'refreshToken') &&
+  typeof data.refreshToken === 'string' &&
+  hasKey(data, 'accessToken') &&
+  typeof data.accessToken === 'string' &&
+  hasKey(data, 'region') &&
+  isRegion(data.region)
