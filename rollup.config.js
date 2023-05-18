0013214e6f97fcfb5773a0c6faebe0419aaa8ca5
johannes-lindgren <14206504+johannes-lindgren@users.noreply.github.com>
2023-05-18 10:22:11 +0200

    Update dependencies and reverting preserveModules

--- rollup.config.js
+++ rollup.config.js
@@ -1,49 +1,34 @@
 import typescript from 'rollup-plugin-typescript2'
 import resolve from '@rollup/plugin-node-resolve'
+import external from 'rollup-plugin-peer-deps-external'
 import commonjs from '@rollup/plugin-commonjs'
 import { visualizer } from 'rollup-plugin-visualizer'
-import summary from 'rollup-plugin-summary'
 import json from '@rollup/plugin-json'
-import pkg from './package.json'
-
-const externalPackages = [
-  ...Object.keys(pkg.peerDependencies ?? {}),
-  ...Object.keys(pkg.dependencies ?? {}),
-]
-// Creating regexes of the packages to make sure subpaths of the
-// packages are also treated as external
-const regexesOfPackages = externalPackages.map(
-  (packageName) => new RegExp(`^${packageName}(/.*)?`),
-)
+import packageJson from './package.json' assert { type: 'json' }
+import filesize from 'rollup-plugin-filesize'
 
 /** @type {import('rollup').RollupOptions} */
 export default {
   input: `./src/index.ts`,
-  external: regexesOfPackages,
   output: [
     {
-      format: 'commonjs',
-      preserveModules: true,
-      preserveModulesRoot: 'src',
-      dir: 'dist',
-      entryFileNames: '[name].cjs',
+      file: packageJson.module,
+      format: 'esm',
       sourcemap: true,
     },
     {
-      format: 'es',
-      preserveModules: true,
-      preserveModulesRoot: 'src',
-      dir: 'dist',
-      entryFileNames: '[name].mjs',
+      file: packageJson.main,
+      format: 'cjs',
       sourcemap: true,
     },
   ],
   plugins: [
+    external(),
     resolve(),
     json(),
     commonjs(),
     typescript(),
-    summary(),
     visualizer(),
+    filesize(),
   ],
 }
