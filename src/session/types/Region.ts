e43447370ab9457b6bac21dc6dca470e9a213d25
Bibiana Sebestianova <b.sebestianova@gmail.com>
2024-01-29 17:12:42 +0100

    fix: use region-helper instead of hardcoded region functions

--- src/session/types/Region.ts
+++ src/session/types/Region.ts
@@ -1,5 +1,9 @@
 // As in https://github.com/storyblok/storyblok-js-client/blob/main/src/sbHelpers.ts
-export type Region = 'eu' | 'us'
+export type Region = 'eu' | 'us' | 'ca' | 'cn' | 'ap'
 
 export const isRegion = (data: unknown): data is Region =>
-  data === 'eu' || data === 'us'
+  data === 'eu' ||
+  data === 'us' ||
+  data === 'cn' ||
+  data === 'ca' ||
+  data === 'ap'
