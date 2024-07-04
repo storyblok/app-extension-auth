becc62803cd7c1314d179958b572cbdd08b21440
Eunjae Lee <hey@eunjae.dev>
2024-07-04 16:36:09 +0200

    remove unused file

--- src/utils/query-params/get-query-params.ts
+++
@@ -1,8 +0,0 @@
-import { URLSearchParams } from 'node:url'
-
-export const getQueryParams = (url: string): URLSearchParams => {
-  const queryParams = url.split('?')[1]
-  const query = new URLSearchParams(queryParams)
-
-  return query
-}
