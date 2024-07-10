d8de8c518a560b11e81fa19b99998055d94e84f3
Bibiana Sebestianova <b.sebestianova@gmail.com>
2024-07-10 11:50:14 +0200

    feat: better test cases

--- src/utils/query-params/get-query-string.test.ts
+++ src/utils/query-params/get-query-string.test.ts
@@ -1,23 +1,51 @@
 import { getQueryFromUrl } from './get-query-string'
 
-describe('getQueryStringFromUrl()', () => {
-  it('should work for urls without trailing slash', () => {
-    const testString = 'https://test.com?hello=2'
-    expect(getQueryFromUrl(testString)).not.toBeUndefined()
+describe('getQueryStringFromUrl() ', () => {
+  describe('positive', () => {
+    it('should retrieve query parameters', () => {
+      const testString = 'https://test.com?hello=2'
+      expect(getQueryFromUrl(testString)).not.toBeUndefined()
+      expect(getQueryFromUrl(testString)).toBeInstanceOf(URLSearchParams)
+      expect(getQueryFromUrl(testString)?.get('hello')).toBe('2')
+    })
+    it('should retrieve query parameters', () => {
+      const testString = '/?hello=2'
+      expect(getQueryFromUrl(testString)).not.toBeUndefined()
+      expect(getQueryFromUrl(testString)).toBeInstanceOf(URLSearchParams)
+      expect(getQueryFromUrl(testString)?.get('hello')).toBe('2')
+    })
+    it('should retrieve query parameters', () => {
+      const testString = '?hello=2'
+      expect(getQueryFromUrl(testString)).not.toBeUndefined()
+      expect(getQueryFromUrl(testString)).toBeInstanceOf(URLSearchParams)
+      expect(getQueryFromUrl(testString)?.get('hello')).toBe('2')
+    })
+    it('should retrieve query parameters', () => {
+      const testString = 'https://test.com/hello/?hello=2'
+      expect(getQueryFromUrl(testString)).not.toBeUndefined()
+      expect(getQueryFromUrl(testString)).toBeInstanceOf(URLSearchParams)
+      expect(getQueryFromUrl(testString)?.get('hello')).toBe('2')
+    })
   })
 
-  it('should work for urls without trailing slash', () => {
-    const testString = '/?hello=2'
-    expect(getQueryFromUrl(testString)).not.toBeUndefined()
-  })
+  describe('negative', () => {
+    it('should not find any query parameters', () => {
+      const testString = 'https://test.com'
+      expect(getQueryFromUrl(testString)).toBeUndefined()
+    })
+    it('should not find any query parameters', () => {
+      const testString = '/hello'
+      expect(getQueryFromUrl(testString)).toBeUndefined()
+    })
 
-  it('should work for urls without trailing slash', () => {
-    const testString = '?hello=2'
-    expect(getQueryFromUrl(testString)).not.toBeUndefined()
-  })
+    it('should not find any query parameters', () => {
+      const testString = ''
+      expect(getQueryFromUrl(testString)).toBeUndefined()
+    })
 
-  it('should work for urls without trailing slash', () => {
-    const testString = 'https://test.com'
-    expect(getQueryFromUrl(testString)).toBeUndefined()
+    it('should not find any query parameters', () => {
+      const testString = 'https://test.com/hello'
+      expect(getQueryFromUrl(testString)).toBeUndefined()
+    })
   })
 })
